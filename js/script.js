b84bd8798e707fb0dfbf5ffc098a6d781f542b1d
gdimran <imran_hossain.gd98@yahoo.com>
2021-10-17 02:05:07 -0800

    Sidebar with toggle options is completed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { $('#sidebarCollapse').on('click', function() { $('#sidebar').toggleClass('active'); }); });
+
+// var container = document.querySelector('#sidebar');
+
+// container.addEventListener('mouseenter', function() {
+//     this.classList.add('active');
+// })
+// container.addEventListener('mouseleave', function() {
+//     this.classList.remove('active');
+// })
+
+
+
+
+
+//$(document).ready(function() { $('#Submenushow').on('click', function() { $('#pageSubmenu').toggleClass('show'); }); });
