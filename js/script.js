bd4a3b735f50c84c12f17155f7dda1d3e954bdf9
gdimran <imran_hossain.gd98@yahoo.com>
2021-10-21 01:05:27 +0600

    Tabs,button,form basic and domains page done.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { $('#sidebarCollapse').on('click', function() { $('#sidebar').toggleClass('active'); }); });
+$(document).ready(function () { $('#sidebarCollapse').on('click', function () { $('#sidebar').toggleClass('active'); }); });
 
 // var container = document.querySelector('#sidebar');
 
@@ -14,3 +14,24 @@ $(document).ready(function() { $('#sidebarCollapse').on('click', function() { $(
 
 
 //$(document).ready(function() { $('#Submenushow').on('click', function() { $('#pageSubmenu').toggleClass('show'); }); });
+
+// Tabs navigation
+const tabs = document.querySelector(".tab-wrapper");
+const tabButton = document.querySelectorAll(".tab-nav-item");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if (id) {
+        tabButton.forEach(btn => {
+            btn.classList.remove("active-tab");
+        });
+        e.target.classList.add("active-tab");
+
+        contents.forEach(content => {
+            content.classList.remove("active-content");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active-content");
+    }
+}
